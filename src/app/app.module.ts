eca952069c5d73be72d94a0cff283298ed160a0f
Brett Scott <brettsamscott@gmail.com>
2022-10-05 19:34:55 +0200

    added pages intro, skills and projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    SkillsComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
