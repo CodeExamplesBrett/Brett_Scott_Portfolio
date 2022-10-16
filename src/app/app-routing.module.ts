ee8ed753937b0fe688db129d982d1c598910499e
Brett Scott <brettsamscott@gmail.com>
2022-10-16 16:28:42 +0200

    add imprint router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: '', component: SkillsComponent},
+  {path: 'imprint', component: ImprintComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     SkillsComponent,
     WorkComponent,
     AboutComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule,
