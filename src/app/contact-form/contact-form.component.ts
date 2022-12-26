d2ce0cb950ab50fc01e53f998dc982718f1dd821
Brett Scott <brettsamscott@gmail.com>
2022-12-26 16:40:53 +0100

    legal css fix

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -12,12 +12,10 @@ export class ContactFormComponent implements OnInit {
   @ViewChild('nameField') nameField: ElementRef;
   @ViewChild('messageField') messageField: ElementRef;
   @ViewChild('emailField') emailField: ElementRef;
-
   @ViewChild('sendButton') sendButton: ElementRef;
 
   emailSent = true;
   fadeOut = false;
-
   emailValid = true;
 
 
@@ -26,14 +24,10 @@ export class ContactFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  async sendMail(){
-    
-    console.log('Sending mail', this.myForm);
-
+  getFormData(){
     let nameField = this.nameField.nativeElement;
     let messageField = this.messageField.nativeElement;
     let emailField = this.emailField.nativeElement;
-
     let sendButton = this.sendButton.nativeElement;
 
     nameField.disabled = true;
@@ -41,38 +35,65 @@ export class ContactFormComponent implements OnInit {
     sendButton.disabled = true;
     emailField.disabled = true;
 
-    
-
     let fd = new FormData();
+
     fd.append('name',nameField.value);
     fd.append('message', messageField.value);
     fd.append('email', emailField.value);
+  
+    this.checkMail(nameField, messageField, sendButton, emailField, fd)
+  }  
 
-    if(emailField.value.toLowerCase()
-    .match(
+
+  checkMail(nameField, messageField, sendButton, emailField, fd){
+
+    if(emailField.value.toLowerCase().match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     ))
     {   
+      this.sendMail(nameField, messageField, sendButton, emailField, fd)
+    } else {
+      this.InvalidEMailWarning(nameField, messageField, sendButton, emailField)
+    }
+  }
+
+
+  async sendMail(nameField, messageField, sendButton, emailField, fd){
         this.emailValid = true;
         // senden
         await fetch('http://brett--scott.com/send_mail/send_mail.php', 
         {
           method: 'POST',
           body: fd
-        }
-        
-        );
+        });
 
-        //text anzeigen :  Nachricht gesendet
-        console.log('mail sent');
+        this.sentMailAnimation();
+        this.resetForm(nameField, messageField, sendButton, emailField);
+  }
 
-        this.emailSent = false;
-        this.fadeOut = true;
-        setTimeout(() => {
-          this.emailSent = true;
-        }, 6000);
-        
+  InvalidEMailWarning(nameField, messageField, sendButton, emailField){
+    // variable "emailValid" removes the class "dnone" to display the warning.
+    this.emailValid = false;
 
+    nameField.disabled = false;
+    messageField.disabled = false;
+    sendButton.disabled = false;
+    emailField.disabled = false;
+
+    emailField.value = ""
+  }
+
+
+  sentMailAnimation(){
+    this.emailSent = false;
+    this.fadeOut = true;
+    setTimeout(() => {
+      this.emailSent = true;
+    }, 6000);
+}
+
+
+  resetForm(nameField, messageField, sendButton, emailField){
         nameField.disabled = false;
         messageField.disabled = false;
         sendButton.disabled = false;
@@ -81,25 +102,5 @@ export class ContactFormComponent implements OnInit {
         nameField.value = ""
         messageField.value =""
         emailField.value = ""
-        
-        console.log("yes")
-
-
-    } else {
-      this.emailValid = false;
-
-        nameField.disabled = false;
-        messageField.disabled = false;
-        sendButton.disabled = false;
-        emailField.disabled = false;
-
-        
-        
-        emailField.value = ""
-    }
-
-    
-
   }
-
 }
